8bd224db1c09a79046254cee5d19cab1e42fda40
mazas7759 <mazas7759@uta.edu.ec>
2021-04-01 23:15:20 -0500

    BackEnd

---
+++ BackEnd/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using BackEnd.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BackEnd
+{
+    public class ApplicationDbContext:DbContext
+    {
+
+        //el constructor va a recibir como parametro la cadena de coneccion
+        // utilizando (inyeccion de dependencias)
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<TarjetaCredito> TarjetaCredito { set; get; }
+
+    }
+}
---
+++ BackEnd/Startup.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace BackEnd
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            //Agregar un objeto DBContext para utilizar SQLServer
+            //para esto se va utilizar "services"
+            // y hacer referncia a la cadena de conexion
+            services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("TarjetaCreditoConncetionsString")));
+
+            //Habilitar CORS, AGREGAR y permitir  que cualquier origen se usae la PI
+            services.AddCors(options => options.AddPolicy("AllowWenApp", builder => builder.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod()));
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            //Usar el middle (CORS) para permitir a la app que accesedan a la web API
+            app.UseCors("AllowWebApp");
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
